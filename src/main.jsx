20d86e083ae7e83392995a64c854ca4ed079c075
RanchoTao <72544580+RanchoTao@users.noreply.github.com>
2026-08-21 13:04:56 +0800

    Add dropdown navigation and research team organization chart
    Restructure WULab navigation, organization chart, research news categories, and contact information.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './AppEnhanced.jsx'
 import './styles.css'
 import './people.css'
 import './brighten.css'
+import './navigation-org.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
